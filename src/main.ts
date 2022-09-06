93c1d762c6ac5207030d81b6df8aace766337c4a
LiuYuWei <455450326@qq.com>
2022-09-06 18:37:21 +0800

    标签样式初始化

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 // 浏览器兼容
 import 'normalize.css';
+// 标签样式初始化
+import '@/assets/styles/common.less';
 const app = createApp(App);
 app.mount('#app');
