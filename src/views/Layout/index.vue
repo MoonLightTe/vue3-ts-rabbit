5d604cc214651708b97b09e85dffd300182d8d09
LiuYuWei <455450326@qq.com>
2022-09-07 10:56:21 +0800

    头部与底部的布局完成

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import AppTopnav from './components/app-topnav.vue';
 import AppHeader from './components/app-header.vue';
+import AppFooter from './components/app-footer.vue'
 </script>
 
 <template>
   <AppTopnav />
   <AppHeader />
+  <AppFooter />
 </template>
 
 <style lang="less" scoped></style>
