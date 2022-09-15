a50e2a4fdc08cdb187059af47806654043e3b523
LiuYuWei <455450326@qq.com>
2022-09-15 23:56:15 +0800

    改变hosts文件将本地的域名映射为一个新的域名

---
+++ src/views/Login/callback.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+  //
+  </script>
+
+  <template>
+    <h1>callback-QQ登录回跳页面测试</h1>
+  </template>
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,16 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 配置开发服务器
+  server:{
+    // qq三方登录的回调url为 http://www.corho.com:8080/#/login/callback、、
+    // vite中配置：www.corho.com:8080
+    host:"www.corho.com",
+    port:8080,
+    open:true, // 帮我们打开浏览器
+    cors:true,// 允许开发时ajax跨域
+    proxy:{} //代理
+  },
   plugins: [vue(
     {
       reactivityTransform:true,
