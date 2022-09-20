fdbb9d881b4374cbf6ff182754e25d769a30f0cd
LiuYuWei <455450326@qq.com>
2022-09-21 00:01:11 +0800

    Teleport 传送作用是将一些标签传送指定的dom元素中

--- src/views/checkout/components/toggleAddress.vue
+++ src/views/checkout/components/toggleAddress.vue
@@ -6,7 +6,7 @@ defineProps<{
   visible: boolean;
 }>();
 const verifyBtn = () => {
-  
+
 };
 const cancelBtn = () => {
   emit('update:visible', false);
@@ -14,6 +14,13 @@ const cancelBtn = () => {
 </script>
 
 <template>
+   <!--
++    fixed 定位 会被祖先元素的 transform、perspective、filter 影响
++    祖先元素 transform、perspective、filter 不为 none的时候
++    fixed定位的参照物会是该祖先元素
++   -->
+ <!-- <div style="transform: translate(0)"> -->
+  <Teleport to="#modul">
   <XtxDialog title="切换收货地址" :visible="visible" @close="cancelBtn">
     <template #default>
       <h1>对话框内容</h1>
@@ -25,6 +32,8 @@ const cancelBtn = () => {
       <XtxButton type="primary" @click="verifyBtn">确认</XtxButton>
     </template>
   </XtxDialog>
+</Teleport>
+  <!-- </div> -->
 </template>
 
 <style lang="less" scoped></style>
